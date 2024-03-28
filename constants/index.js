66e516bd1d32f282992d23db136798579cd0011b
Kushal Dongre <kushz19@gmail.com>
2024-03-28 11:58:48 +0530

    Add contact

--- constants/index.js
+++ constants/index.js
@@ -68,3 +68,9 @@ export const PROJECTS = [
     technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
   },
 ];
+
+export const CONTACT = {
+  address: "767 Fifth Avenue, New York, NY 10153 ",
+  phoneNo: "+12 4555 666 00 ",
+  email: "me@example.com",
+};
